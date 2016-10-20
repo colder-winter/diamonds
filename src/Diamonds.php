f6c0d95756b22a9d2d39528634f46228ffb94e79
ColderWinter <8162782@qq.com>
2016-10-20 15:32:50 +0800

    创建Diamonds

---
+++ src/Diamonds.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/10/20
+ * Time: 15:23
+ */
+
+namespace Colder;
+
+
+class Diamonds
+{
+    public static function info()
+    {
+        echo 'Diamond Rings';
+    }
+}
